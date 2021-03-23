a2b25d3f8f453d9ad2a4b871e8d54dd8f2fbd818
Marcus <mdb013@protonmail.com>
2021-03-24 09:22:53 +1100

    Added a more compatable codebase and used c header files

--- cli-clock.cpp
+++ cli-clock.cpp
@@ -1,8 +1,12 @@
+// C++ Libs
 #include <iostream>
-#include <ctime>
 #include <csignal>
 #include <thread>
 #include <chrono>
+
+// C Headers
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
@@ -12,14 +16,12 @@ void sleep(int sleeptime) {
 }
 
 int main() {
-    // Prepares the signal handler
-
-
     while (true) {
-            time_t now = time(0); // time variable
-            char* tmnow = ctime(&now); // converts the time variable to a string to be printed
-            system("clear");
-            std::cout << tmnow << endl;
-            sleep(1000); // Sleeps the loop for 1 second
-        }
+        time_t rawtime;
+        time (&rawtime);
+        printf ("%s", ctime (&rawtime));
+        sleep(1000); // Sleeps the loop for 1 second
+        system("clear");
+
+    }
 }
