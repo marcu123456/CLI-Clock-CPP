31ad2cdd1b2db37b1d17fc5093dae302915c3286
Marcus <mdb013@protonmail.com>
2021-04-01 10:13:46 +1100

    Major stuffs

--- cli-clock.cpp
+++ cli-clock.cpp
@@ -1,8 +1,14 @@
-// C++ Libs
+/*
+* Made by marcu
+* Find me on GitHub
+* https://github.com/marcu123456
+*/
+
+// C++ Headers
 #include <iostream>
 #include <csignal>
-#include <thread>
-#include <chrono>
+#include <thread> // Used to sleep the thread
+#include <chrono> // Used for getting the time
 
 // C Headers
 #include <stdio.h> // For outputing the time to screen
@@ -10,29 +16,29 @@
 
 using namespace std;
 
-void sleep(int sleeptime) {
-    // In miliseconds
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleeptime));
+void sleep(int seconds) {
+    // In seconds
+    seconds * 1000; // Converts to miliseconds.
+    std::this_thread::sleep_for(std::chrono::milliseconds(seconds));
 }
 
 int main() {
     while (true) {
-
-    time_t t ;
-    struct tm *temp ;
-    char currtime[50];
-    time( &t );
-    temp = localtime( &t );
-      
-    // using strftime to display time
-    strftime(currtime, sizeof(currtime), "%x - %I:%M%p", temp);
-    
-    printf("%s\n", currtime );
-    
-    #pragma region clear
-        sleep(1000); // Sleeps the loop for 1 second
+        time_t t ;
+        struct tm *temp ;
+        char currtime[50];
+        time( &t );
+        temp = localtime( &t );
+        
+        // using strftime to change time structure
+        strftime(currtime, sizeof(currtime), "%x - %I:%M%p", temp);
+        
+        printf("%s\n", currtime );
+        
+        sleep(1); // Sleeps the loop for 1 second
         system("clear");
-    #pragma endregion
+
     }
+    return 0;
 }
 
