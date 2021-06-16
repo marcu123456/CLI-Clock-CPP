8bfdbe645d8d92507642d1cccc29e1688dad2a91
Marcus <mdb013@protonmail.com>
2021-06-16 13:59:59 +1000

    Made the code more readalbe and easier for me if I eve go back to it

--- cli-clock.cpp
+++ cli-clock.cpp
@@ -5,10 +5,10 @@
 */
 
 // C++ Headers
-#include <iostream>
+#include <iostream> // Used for certain outputs despite stdio.h's printf() funciton is used to output the text to the screen
 #include <csignal>
-#include <thread> // Used to sleep the thread
-#include <chrono> // Used for getting the time
+#include <thread> // Used to sleep the thread and make the program temporararily halt
+#include <chrono> // Used for getting time values for the sleepSeconds() function
 
 // C Headers
 #include <stdio.h> // For outputing the time to screen
@@ -16,27 +16,30 @@
 
 using namespace std;
 
-void sleep(int seconds) {
+void sleepSeconds(int seconds) {
     // In seconds
-    seconds * 1000; // Converts to miliseconds.
-    std::this_thread::sleep_for(std::chrono::milliseconds(seconds));
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+void cls() {
+    system("clear"); // Enters the value in the function and clears the terminal
 }
 
 int main() {
     while (true) {
-        time_t t ;
-        struct tm *temp ;
-        char currtime[50];
+        time_t t ; // Time variable
+        struct tm *temp ; // Forgot what this is, I doubt I will find out, describes programming pretty well.
+
+        char currtime[50]; // what is printed to the screen.
         time( &t );
         temp = localtime( &t );
         
         // using strftime to change time structure
-        strftime(currtime, sizeof(currtime), "%x - %I:%M%p", temp);
+        strftime(currtime, sizeof(currtime), "%x - %I:%M%p", temp); // Formats the time into a readable format
         
-        printf("%s\n", currtime );
+        printf("\n%s", currtime ); // Changes the format and outputs the clock
         
-        sleep(1); // Sleeps the loop for 1 second
-        system("clear");
+        sleepSeconds(1); // Sleeps the loop for 1 second
+        cls(); // Calls the clearing function
 
     }
     return 0;
